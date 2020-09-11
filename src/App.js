62a8f54693e8eb193145a44fa4ff7e71b5874ee5
msksaul <64514098+msksaul@users.noreply.github.com>
2020-09-10 18:19:02 -0600

    Payment Componet

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
+import Payment from './Payment';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { authorization } from './firebase';
 import { useStateValue } from './StateProvider';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe('pk_test_51HPz5HJXhhLAeb58McRm29vfmg4e9bmBA0kWo8ODAdfCpCXdzFfbECpP2tT2O3HKXmOOYBVlKObaMLy0NIz1uVn700L4zg6v6j');
 
 function App() {
 
@@ -43,6 +48,12 @@ function App() {
           <Route path='/checkout'>
             <Header/>
             <Checkout/>
+          </Route>
+          <Route path='/payment'>
+            <Header/>
+            <Elements stripe={promise}>
+              <Payment/>
+            </Elements>
           </Route>
           <Route path='/'>
             <Header/>
--- src/Checkout.js
+++ src/Checkout.js
@@ -6,7 +6,7 @@ import { useStateValue } from './StateProvider';
 
 function Checkout() {
 
-  const [{ basket }] = useStateValue();
+  const [{ basket, user }] = useStateValue();
 
   return(
     <div className='checkout'>
@@ -14,6 +14,7 @@ function Checkout() {
         <img className='checkout__ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' alt=''/>
 
         <div>
+          <h3>Hello {user ? user.email : 'Guest'}</h3>
           <h2 className='checkout__title'>Your Shopping Basket</h2>
             {basket?.map(item => (
               <CheckoutProduct
